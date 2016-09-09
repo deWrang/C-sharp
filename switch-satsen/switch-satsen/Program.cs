39807e9ce0da55b626abf8a6ee8985dd985e2407
deWrang <elliot.dewrang@gmail.com>
2016-09-09 12:19:06 +0200

    Add files via upload

---
+++ switch-satsen/switch-satsen/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_satsen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            //Övning 1, Csharpskolan övning om switch-satsen
+            //Elliot de Wrang den 9 sept 2016
+
+            Console.WriteLine("Mata in ett tal mellan 1-4");
+            int tal = int.Parse(Console.ReadLine());
+
+            switch (tal){
+
+                case 1:
+                    Console.WriteLine("Ett");
+                    break;
+                case 2:
+                    Console.WriteLine("Två");
+                    break;
+                case 3:
+                    Console.WriteLine("Tre");
+                    break;
+                case 4:
+                    Console.WriteLine("Fyra");
+                    break;
+                default:
+                    Console.WriteLine("Felaktigt alternativ");
+                    break;
+            }
+            
+            Console.ReadKey();
+            */
+            /*
+            //Elliot de Wrang den 9 sept 2016
+            //Övning 2, Csharpskolan övning om switchsatsen
+            Console.WriteLine("Vart vill du gå, N, S, E, W");
+            string val = (Console.ReadLine());
+
+            switch (val)
+            {
+                case "N":
+                    Console.WriteLine("You walk north");
+                    break;
+                case "S":
+                    Console.WriteLine("You walk south");
+                    break;
+                case "E":
+                    Console.WriteLine("You walk  east");
+                    break;
+                case "W":
+                    Console.WriteLine("You walk west");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand");
+                    break;
+            }
+            Console.ReadKey();
+            */
+        }
+    }
+}
